58408a5f47efccf7781abf43047ac940127114f6
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-09-01 22:13:29 +0300

    setup chats model and changed to built in django auth user

--- messaging_app/main_app/models.py
+++ messaging_app/main_app/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-from django.db import settings
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-User = settings.AUTH_USER_MODEL
+
+class Chat(models.Model):
+    name = models.CharField(max_length=40)
+    is_group = models.BooleanField(default=False)
+    participants = models.ManyToManyField(User, through='ChatMember', related_name='chats')
+
+    class Meta:
+        db_table = 'chats'
+
+    def __str__(self):
+        return self.name
