1f87ebf834a98278dca69576b11b265c3722201f
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-09-03 15:00:48 +0300

    removed seceret in settings

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -23,7 +23,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g=gbmzo8bn5jjg91lgo7%vk9ct3!tq2s1pyuxbgs*$3(5r6+9^'
+SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
