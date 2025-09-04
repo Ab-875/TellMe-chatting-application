a60e82f73932df3036747f1e66869f62f4c7b6f3
Sayed Abdulla <Abdulla.s2002@gmail.com>
2025-09-04 10:48:19 +0300

    added edit to urls

--- messaging_app/main_app/urls.py
+++ messaging_app/main_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
-from . views import MyChatsPageView, CreateChatView, ChatDetailPageView
+from . views import MyChatsPageView, CreateChatView, ChatDetailPageView, ChatUpdateView
 
 urlpatterns = [
    path("admin/", admin.site.urls),
    path("chats/", MyChatsPageView.as_view(), name="my_chats_page"),
-   path("chats/<int:chat_id>/", ChatDetailPageView.as_view(), name="chat_detail_page"),
    path("chats/new/", CreateChatView.as_view(), name="create_chat"),
+   path("chats/<int:chat_id>/", ChatDetailPageView.as_view(), name="chat_detail_page"),
+   path("chats/<int:chat_id>/edit/", ChatUpdateView.as_view(), name="chat_edit"),
 ]
